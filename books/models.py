a92a43698a01555993b9a2e4b5b458f4fcf2a376
Juliana Ferraz <ferrazjsc@gmail.com>
2023-05-02 17:10:53 -0300

    feat: book category and publisher models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Book(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=127)
+    author= models.CharField(max_length=127)
+    description=models.TextField(blank=True, null=True)
+    category = models.ForeignKey(
+        "categories.Category", on_delete=models.CASCADE, related_name="books"
+    )
+    publish_date = models.CharField(max_lenght=10)
+    number_pages = models.IntegerField()
+    language= models.CharField(max_legth=20)
+    publisher=models.ForeignKey(
+        "publishing_company.Publisher", on_delete=models.CASCADE, related_name="books"
+    )
+    isbn=models.CharField(max_length=13)
+    followers=models.ManyToManyField(
+        "users.User",
+        related_name="following_books",
+    )
--- publishing_company/models.py
+++ publishing_company/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Publisher(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    Name = models.CharField(max_length=127)
