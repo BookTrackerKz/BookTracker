ef27ee6ab9808d716f89300aa6f1e0987c8d87da
Douglas <dougbmth@hotmail.com>
2023-05-02 19:25:06 -0300

    feat models copies, loans

---
+++ copies/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Copy
+import uuid
+
+
+class CopySerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, read_only=True
+    )
+
+    class Meta:
+        model = Copy
+        fields = ["is_available", "classification_code", "book_id"]
--- publishing_company/models.py
+++ publishing_company/models.py
@@ -3,6 +3,5 @@ import uuid
 
 
 class Publisher(models.Model):
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    Name = models.CharField(max_length=127)
+    name = models.CharField(max_length=127)
