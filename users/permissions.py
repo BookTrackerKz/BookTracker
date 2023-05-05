17dc48ff310b5b44bd0bd20d68200f8861ca363d
samirdourado <samir3500@gmail.com>
2023-05-05 16:27:54 -0300

    Fix: Permissions CRUD detailView

--- users/permissions.py
+++ users/permissions.py
@@ -2,8 +2,8 @@ from rest_framework import permissions
 from .models import User
 from rest_framework.views import View
 
-    
-class IsAllowedUserToRetrieveAndModify(permissions.BasePermission):
+
+class OnlyUserCompanyCanAcces(permissions.BasePermission):
 
     def has_permission(self, request, view):
 
@@ -11,18 +11,20 @@ class IsAllowedUserToRetrieveAndModify(permissions.BasePermission):
             if request.user.is_superuser:
                 return True
             else:
-                if request.method in permissions.SAFE_METHODS:
+                if request.method in permissions.SAFE_METHODS and request.user.is_staff:
                     return True
         return False
 
-class IsEstudentOwner(permissions.BasePermission):   
+
+class IsEstudentOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view: View, obj: User) -> bool:
         if request.user.is_authenticated:
             if request.user.is_superuser:
                 return True
-            if request.method in permissions.SAFE_METHODS or request.method == "PATCH":
-                if request.user.is_staff or request.method == "DELETE":
-                        return True
-                if obj.id == request.user.id:
-                    return True
+            if request.method in permissions.SAFE_METHODS and request.user.is_staff or request.user.is_staff and request.method == "PATCH":
+                return True
+            if obj.id == request.user.id and request.method != "DELETE":
+                return True
+            if request.user.is_staff and request.method == "DELETE":
+                return True
